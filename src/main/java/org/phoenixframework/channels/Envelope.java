347119e1cbec0b9230974384551ff75bc41ef8a0
grigor-aramyan <grigori.aramyan@gmail.com>
2018-01-19 04:45:30 +0400

    add ignoreProperties annotation

--- src/main/java/org/phoenixframework/channels/Envelope.java
+++ src/main/java/org/phoenixframework/channels/Envelope.java
@@ -1,9 +1,10 @@
 package org.phoenixframework.channels;
 
-
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Envelope {
     @JsonProperty
     private String topic;
